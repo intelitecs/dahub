13e7ee069a27996efdd59888793eac899f9ea4e3
jarodez <jarode@me.com>
2017-10-24 00:42:28 +0000

    Angular material added

--- app/javascript/dahub/app/app.component.ts
+++ app/javascript/dahub/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   //templateUrl: './app.component.html',
   template: `<h1>Welcome to {{name}} from Angular</h1>
+  <button mat-button> Basic Button</button>
 `,
   styles: ['h1 {color: blue;}']
   //styleUrls:['app.component.css']
--- app/javascript/dahub/app/companies/companies.module.ts
+++ app/javascript/dahub/app/companies/companies.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {CompaniesComponent} from "./companies.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
     id: 'companies-module',
-    imports:[BrowserModule],
+    imports:[BrowserModule, FormsModule, ReactiveFormsModule],
     declarations:[CompaniesComponent],
     exports:[CompaniesComponent],
     providers:[],

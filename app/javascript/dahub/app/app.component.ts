3d652c125e1baf032dbbfeb9c65e171f8838c127
jarodez <jarode@me.com>
2017-10-24 10:30:42 +0000

    Material Design toolbar set up

--- app/javascript/dahub/app/app.component.ts
+++ app/javascript/dahub/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{name}} from Angular</h1>
-    <button mat-button> Basic Button</button>
-    <h2 class="alert-success">The application works</h2>
+  template: `
+    
+    <mat-sidenav-container>
+        <mat-toolbar color="primary">
+            <mat-icon (click)="sidenav.toggle()">menu</mat-icon>
+            <span>Dahub Admin</span>
+        </mat-toolbar>
+        <mat-sidenav #sidenav mode="side" opened="true">
+            
+        </mat-sidenav>
+        <h1>Welcome to {{name}} from Angular</h1>
+        <button mat-button> Basic Button</button>
+        <h2 class="alert-success">The application works</h2>
+    </mat-sidenav-container>
+    
+    
+    
 `,
   styles: ['h1 {color: blue;}']
   //styleUrls:['app.component.css']
--- app/javascript/dahub/app/app.module.ts
+++ app/javascript/dahub/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {MatButtonModule} from "@angular/material";
+import {MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule} from "@angular/material";
+import {BrowserAnimationsModule, NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, MatButtonModule
+    BrowserModule, MatButtonModule, MatSidenavModule, MatIconModule,
+    BrowserAnimationsModule, NoopAnimationsModule, MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]

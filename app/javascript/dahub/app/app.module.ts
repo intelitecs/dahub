804fa6a013b5f9576edc96378289cbd6bbf94ab4
jarodez <jarode@me.com>
2017-10-30 03:38:27 +0000

    Changed default format to json

--- app/javascript/dahub/app/app.module.ts
+++ app/javascript/dahub/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import {
--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,32 @@
 Rails.application.routes.draw do
 
-  resources :cotisations
-  resources :advantages
-  resources :contracts
-  resources :identity_cards
-  resources :observations
-  resources :qualifications
-  resources :handicaps
-  resources :family_members
-  resources :employees
-  resources :accountants
-  resources :bank_accounts
-  resources :managers
-  resources :charges
-  resources :etablissement_activities
-  resources :etablissements
-  resources :activities
-  resources :juridic_forms
-  resources :companies
-  resources :company_registry_documents
-  resources :user_roles
-  resources :profiles
-  resources :users
-  resources :user_profiles
-  resources :addresses
-  resources :cities
-  resources :countries
-  resources :roles
+  resources :cotisations, defaults: {format: :json}
+  resources :advantages,  defaults: {format: :json}
+  resources :contracts, defaults: {format: :json}
+  resources :identity_cards, defaults: {format: :json}
+  resources :observations, defaults: {format: :json}
+  resources :qualifications, defaults: {format: :json}
+  resources :handicaps, defaults: {format: :json}
+  resources :family_members, defaults: {format: :json}
+  resources :employees, defaults: {format: :json}
+  resources :accountants, defaults: {format: :json}
+  resources :bank_accounts, defaults: {format: :json}
+  resources :managers, defaults: {format: :json}
+  resources :charges, defaults: {format: :json}
+  resources :etablissement_activities, defaults: {format: :json}
+  resources :etablissements, defaults: {format: :json}
+  resources :activities, defaults: {format: :json}
+  resources :juridic_forms, defaults: {format: :json}
+  resources :companies, defaults: {format: :json}
+  resources :company_registry_documents, defaults: {format: :json}
+  resources :user_roles, defaults: {format: :json}
+  resources :profiles, defaults: {format: :json}
+  resources :users, defaults: {format: :json}
+  resources :user_profiles, defaults: {format: :json}
+  resources :addresses, defaults: {format: :json}
+  resources :cities, defaults: {format: :json}
+  resources :countries, defaults: {format: :json}
+  resources :roles, defaults: {format: :json}
   root 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

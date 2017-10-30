335199ad596917e38a2802880790d13f977f9f22
jarodez <jarode@me.com>
2017-10-30 04:04:47 +0000

    Change config 3

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,16 +1,45 @@
 const environment = require('./environment');
 const webpack = require('webpack');
-//module.exports = environment.toWebpackConfig()
-/*
+
 
 module.exports = {
-    plugins: [
-
-    ]
+    devServer: {
+    },
+    devtool: 'source-map',
+    entry: {
+        //"ym.min": "./src/bootstrap.ts",
+    },
+    module: {
+        /*rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    },
+                    {
+                        loader: "tslint-loader",
+                        enforce: "pre"
+                    }
+                ]
+            }
+        ],*/
+        output: {
+            filename: '[name].js',
+            path: './dist'
+        },
+        plugins: [],
+        /*resolve: {
+            modules: [
+                "node_modules",
+                path.join(__dirname, 'node_modules')
+            ],
+            extensions: [".webpack.js", ".web.js", ".ts", ".js", "*"],
+            enforceExtension: false,
+            alias: {
+                'handlebars': 'handlebars/runtime.js'
+            }
+        }*/
+    }
 };
-*/
-
-
-
-
 

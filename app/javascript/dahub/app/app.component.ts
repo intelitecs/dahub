d46458a90da2c725cab05f836206f1709b90511f
jarodez <jarode@me.com>
2017-10-30 08:45:43 +0000

    Layout set up

--- app/javascript/dahub/app/app.component.ts
+++ app/javascript/dahub/app/app.component.ts
@@ -1,18 +1,21 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 @Component({
   selector: 'app-root',
-  template: `      
-          <h1>Bienvenu sur le server {{name}}</h1>
-          <h1>
+  template: ` 
+      <div class="center jumbotron">
+          <h1> {{name | uppercase}} </h1>
+          <h2>
               Visitez <a class="alert-success" href="https://dahub-client.herokuapp.com">Dahub Client</a> pour voir le client.
-          </h1>
+          </h2>
+      </div>
+      
   `,
   styles: ['h1 {color: blue;} a{text-decoration: none; color: blue;}']
   //styleUrls:['app.component.css']
 })
 
 export class AppComponent {
-  name = 'Dahub Admin';
+  name = 'Dahub Admin Server';
 
 
     @Output()

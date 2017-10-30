ce003e62c05a4c6f64984345b0fb504d258e4176
jarodez <jarode@me.com>
2017-10-30 04:29:38 +0000

    Change config 5

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -9,34 +9,7 @@ module.exports = {
     entry: {
         //"ym.min": "./src/bootstrap.ts",
     },
-    module: {
-        /*rules: [
-            {
-                test: /\.ts$/,
-                use: [
-                    {
-                        loader: "ts-loader"
-                    },
-                    {
-                        loader: "tslint-loader",
-                        enforce: "pre"
-                    }
-                ]
-            }
-        ],*/
-        output: { filename: '[name]-[chunkhash].js' },
-        plugins: [],
-        /*resolve: {
-            modules: [
-                "node_modules",
-                path.join(__dirname, 'node_modules')
-            ],
-            extensions: [".webpack.js", ".web.js", ".ts", ".js", "*"],
-            enforceExtension: false,
-            alias: {
-                'handlebars': 'handlebars/runtime.js'
-            }
-        }*/
-    }
+    output: { filename: 'bundle.js' },
+
 };
 

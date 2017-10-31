f8b9a6ca483b7a9254f85f945ea7a76df7e031ea
jarodez <jarode@me.com>
2017-10-31 08:15:56 +0000

    Sign up form set up

--- config/routes.rb
+++ config/routes.rb
@@ -8,33 +8,33 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  resources :cotisations, defaults: {format: :json}
-  resources :advantages,  defaults: {format: :json}
-  resources :contracts, defaults: {format: :json}
-  resources :identity_cards, defaults: {format: :json}
-  resources :observations, defaults: {format: :json}
-  resources :qualifications, defaults: {format: :json}
-  resources :handicaps, defaults: {format: :json}
-  resources :family_members, defaults: {format: :json}
-  resources :employees, defaults: {format: :json}
-  resources :accountants, defaults: {format: :json}
-  resources :bank_accounts, defaults: {format: :json}
-  resources :managers, defaults: {format: :json}
-  resources :charges, defaults: {format: :json}
-  resources :etablissement_activities, defaults: {format: :json}
-  resources :etablissements, defaults: {format: :json}
-  resources :activities, defaults: {format: :json}
-  resources :juridic_forms, defaults: {format: :json}
-  resources :companies, defaults: {format: :json}
-  resources :company_registry_documents, defaults: {format: :json}
-  resources :user_roles, defaults: {format: :json}
-  resources :profiles, defaults: {format: :json}
-  resources :users, defaults: {format: :json}
-  resources :user_profiles, defaults: {format: :json}
-  resources :addresses, defaults: {format: :json}
-  resources :cities, defaults: {format: :json}
-  resources :countries, defaults: {format: :json}
-  resources :roles, defaults: {format: :json}
+  resources :cotisations, defaults: {format: :html}
+  resources :advantages,  defaults: {format: :html}
+  resources :contracts, defaults: {format: :html}
+  resources :identity_cards, defaults: {format: :html}
+  resources :observations, defaults: {format: :html}
+  resources :qualifications, defaults: {format: :html}
+  resources :handicaps, defaults: {format: :html}
+  resources :family_members, defaults: {format: :html}
+  resources :employees, defaults: {format: :html}
+  resources :accountants, defaults: {format: :html}
+  resources :bank_accounts, defaults: {format: :html}
+  resources :managers, defaults: {format: :html}
+  resources :charges, defaults: {format: :html}
+  resources :etablissement_activities, defaults: {format: :html}
+  resources :etablissements, defaults: {format: :html}
+  resources :activities, defaults: {format: :html}
+  resources :juridic_forms, defaults: {format: :html}
+  resources :companies, defaults: {format: :html}
+  resources :company_registry_documents, defaults: {format: :html}
+  resources :user_roles, defaults: {format: :html}
+  resources :profiles, defaults: {format: :html}
+  resources :users, defaults: {format: :html}
+  resources :user_profiles, defaults: {format: :html}
+  resources :addresses, defaults: {format: :html}
+  resources :cities, defaults: {format: :html}
+  resources :countries, defaults: {format: :html}
+  resources :roles, defaults: {format: :html}
   root 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

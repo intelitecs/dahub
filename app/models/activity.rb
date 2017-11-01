680b1688506593c1d4b5d3b1e6e0ca438e29b216
jarodez <jarode@me.com>
2017-11-01 07:46:24 +0000

    Set Activities Etablissement and Etablissement_Activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ApplicationRecord
-  #has_many :etablissement_activities
-  #has_many :etablissements, through: :etablissement_activities
+  has_many :etablissement_activities
+  has_many :etablissements, through: :etablissement_activities
   validates_presence_of :code, :nature, :sector
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -3,7 +3,7 @@ class Company < ApplicationRecord
   #belongs_to :juridic_form, required: true
   #belongs_to :country
   #belongs_to :address
-  #has_many   :etablissements
+  has_many   :etablissements
   #has_many   :charges
   #has_one    :manager
   #has_one    :bank_account

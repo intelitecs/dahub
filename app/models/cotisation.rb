11caaec8c23b1c811bfda425024d64d8f9234d78
jarodez <jarode@me.com>
2017-11-01 04:08:38 +0000

    Reset associations step employee

--- app/models/cotisation.rb
+++ app/models/cotisation.rb
@@ -1,5 +1,5 @@
 class Cotisation < ApplicationRecord
-  #belongs_to :company, required: true
-  #belongs_to :employee, required: true
+  belongs_to :company, required: true
+  belongs_to :employee, required: true
   validates_presence_of :periodStart, :periodEnd, :declaredAmount, :payedAmount
 end

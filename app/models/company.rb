e6e3386b0768281f83e8fe3d6a61dd6df4739eb0
jarodez <jarode@me.com>
2017-11-01 03:49:06 +0000

    Reset associations step 1

--- app/models/company.rb
+++ app/models/company.rb
@@ -7,7 +7,7 @@ class Company < ApplicationRecord
   #has_many   :charges
   #has_one    :manager
   #has_one    :bank_account
-  #has_one    :accountant
+  has_one    :accountant
   #has_many :contracts
   #has_many :employees, through: :contracts
   validates_presence_of :name, :cnps, :cc, :acte_const_at, :juridic_form, :impotRegime, :impotCenter, :socialReason, :num_acte_const
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ApplicationRecord
   before_save {self.name = name.downcase}
-  #has_many :profile_roles
-  #has_many :roles, through: :profile_roles
+  has_many :profile_roles
+  has_many :roles, through: :profile_roles
   validates_presence_of :name
 end

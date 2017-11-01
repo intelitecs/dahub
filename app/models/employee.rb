c63284a622c2e333c4ffa16aca1b96011da2104e
jarodez <jarode@me.com>
2017-11-01 04:45:44 +0000

    Reset associations step Company 2

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,6 +7,6 @@ class Employee < ApplicationRecord
   has_many :qualifications
   has_many :handicaps
   has_many :advantages
-  has_many :cotisations
+  #has_many :cotisations
   validates_presence_of :lastname, :firstname, :birthdate, :matricule, :nationality, :matrimonial_situation, :hireAt
 end

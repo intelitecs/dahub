a88cd6da5b38bf8d854dfa992cd7bfd09637826f
jarodez <jarode@me.com>
2017-11-01 09:46:12 +0000

    Set family_members observations qualifications handicaps in Employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,10 +1,10 @@
 class Employee < ApplicationRecord
   belongs_to :company, required: true
   has_many :contracts
-  #has_many :family_members
-  #has_many :observations
-  #has_many :qualifications
-  #has_many :handicaps
+  has_many :family_members
+  has_many :observations
+  has_many :qualifications
+  has_many :handicaps
   has_many :advantages
   has_many :cotisations
   validates_presence_of :lastname, :firstname, :birthdate, :matricule, :nationality, :matrimonial_situation, :hireAt
